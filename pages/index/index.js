f12014f46e0f300f880b4e98a37a0018005cb74f
BBBhr <1148562816@qq.com>
2017-01-13 18:01:30 +0800

    add index

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,34 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    imgUrls: [
+      'http://127.0.0.1/images/%E8%BD%AE%E6%92%AD/01.jpg',
+      'http://127.0.0.1/images/%E8%BD%AE%E6%92%AD/02.jpg',
+      'http://127.0.0.1/images/%E8%BD%AE%E6%92%AD/03.jpg'
+    ],
+    martInfos:[{
+      left:'店铺信息',
+      right:''
+    },{
+      left:'店铺类型：',
+      right:'西式快餐'
+    },{
+      left:'电话：',
+      right:'0512-68186015'
+    },{
+      left:'起送价：',
+      right:'￥20'
+    },{
+      left:'外送费：',
+      right:'￥6（满99可免运费）'
+    },{
+      left:'外送范围：',
+      right:'5公里'
+    },{
+      left:'店铺地址：',
+      right:'高新区长江路463号绿宝广场一层'
+    }]
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
 })
